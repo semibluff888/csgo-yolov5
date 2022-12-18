22fd982b042077a1d391b801a1af8f3dafb30c36
semibluff888 <78004964+semibluff888@users.noreply.github.com>
2022-12-18 11:27:01 +0800

    small changes in comments.

--- csgo/cs_model.py
+++ csgo/cs_model.py
@@ -7,7 +7,6 @@ from utils.general import check_img_size
 def load_model(args):
     device = select_device('' if args.use_cuda else 'cpu')
     model = DetectMultiBackend(args.model_path, device=device, dnn=False, fp16=False)
-    # model = DetectMultiBackend(weights, device=device, dnn=False, fp16=False)
     stride, names, pt = model.stride, model.names, model.pt
     imgsz = check_img_size((args.imgsz, args.imgsz), s=stride)
     model.warmup(imgsz=(1, 3, *imgsz))
